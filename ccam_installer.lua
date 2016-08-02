7e1c5b15a20235f1d4a3bcf266f9eea77363ab05
ACharLuk <acharluk@gmail.com>
2016-08-02 18:25:55 +0200

    Rewritten installer

--- ccam_installer.lua
+++ ccam_installer.lua
@@ -1,23 +1,36 @@
 
-local deleteFiles = {
-	"ccam_installer",
-	"grd",
-	"Minimal-Install",
-	".gitignore",
-	"README.md",
-	".ccam/app/ccam/README.md",
-	".ccam/lib/ccam_lib/README.md"
+local base_url = "https://raw.github.com/CCAppMarket/"
+
+local files = {
+	["/startup"] = "Minimal-Install/master/startup",
+	["/.ccam/load.lua"] = "Minimal-Install/master/.ccam/load.lua",
+	["/.ccam/config.cfg"] = "Minimal-Install/master/.ccam/config.cfg",
+	["/.ccam/bin/ccam"] = "Minimal-Install/master/.ccam/bin/ccam",
+
+	["/.ccam/app/ccam/main.lua"] = "AppRepository/stable/ccam/main.lua",
+	["/.ccam/app/ccam/app.json"] = "AppRepository/stable/ccam/app.json",
+
+	["/.ccam/lib/ccam/main.lua"] = "LibRepository/stable/ccam/main.lua",
+	["/.ccam/lib/ccam/lib.json"] = "LibRepository/stable/ccam/lib.json",
+
+	["/.ccam/lib/json/main.lua"] = "LibRepository/stable/json/main.lua",
+	["/.ccam/lib/json/lib.json"] = "LibRepository/stable/json/lib.json",
+
+	["/.ccam/lib/net/main.lua"] = "LibRepository/stable/net/main.lua",
+	["/.ccam/lib/net/lib.json"] = "LibRepository/stable/net/lib.json",
+
+	["/.ccam/lib/utils/main.lua"] = "LibRepository/stable/utils/main.lua",
+	["/.ccam/lib/utils/lib.json"] = "LibRepository/stable/utils/lib.json"
+
 }
 
-file = fs.open("grd","w")
-file.write(http.get("https://raw.githubusercontent.com/CCAppMarket/Installer/master/grd").readAll())
-file.close()
-
-shell.run("grd CCAppMarket Minimal-Install / . .")
-shell.run("mv Minimal-Install/* /")
-
-for _, v in pairs(deleteFiles) do
-	fs.delete(v)
+for k, v in pairs(files) do
+	print("Downloading: " .. k)
+	local f = fs.open(k, 'w')
+	local d = http.get(base_url .. v).readAll()
+	f.write(d)
+	f.close()
 end
 
+fs.delete("ccam_install")
 os.reboot()
