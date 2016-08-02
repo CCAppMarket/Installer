2c67c81e96b372db58c31bc62a04c2d7d20808ea
ACharLuk <acharluk@gmail.com>
2016-08-02 17:31:44 +0200

    Removing files

--- ccam_installer.lua
+++ ccam_installer.lua
@@ -1,4 +1,14 @@
 
+local deleteFiles = {
+	"ccam_installer",
+	"grd",
+	"Minimal-Install",
+	".gitignore",
+	"README.md",
+	".ccam/app/ccam/README.md",
+	".ccam/lib/ccam_lib/README.md"
+}
+
 file = fs.open("grd","w")
 file.write(http.get("https://raw.githubusercontent.com/CCAppMarket/Installer/master/grd").readAll())
 file.close()
@@ -6,7 +16,8 @@ file.close()
 shell.run("grd CCAppMarket Minimal-Install / . .")
 shell.run("mv Minimal-Install/* /")
 
-fs.delete("README.md")
-fs.delete("grd")
-fs.delete("Minimal-Install")
-fs.delete(".gitignore")
+for _, v in pairs(deleteFiles) do
+	fs.delete(v)
+end
+
+os.reboot()
